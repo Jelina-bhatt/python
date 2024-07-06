8e6f6eb953c2c029a70aeb16fe04b367e7ac0d83
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-07-06 22:22:27 +0545

    Create Dictionary.py

---
+++ folder1/Dictionary.py
@@ -0,0 +1,15 @@
+# Create a dictionary
+person = {
+    "name": "jelina",
+    "age": 20,
+    "city": "Kathmandu"
+}
+print(person)
+
+# Access elements
+print(person["name"])  # jelina 
+print(person["age"])   # 20
+
+# Modify elements
+person["age"] = 26
+print(person)
