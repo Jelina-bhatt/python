2b1e9526a36129a25da8adbeba30c321e1bdce76
Jelina Bhatt <155757956+Jelina-bhatt@users.noreply.github.com>
2024-07-11 23:59:37 +0545

    Create List.py

---
+++ folder1/List.py
@@ -0,0 +1,6 @@
+# Lists
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")    # Adding an element to the list
+fruits.remove("banana")    # Removing an element from the list
+
+print(fruits)
