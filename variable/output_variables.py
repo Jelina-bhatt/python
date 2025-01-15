8e193e063934775440747aef7b2288b80d0cb503
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-15 20:44:26 +0545

    output variables modified

--- variable/output_variables.py
+++ variable/output_variables.py
@@ -11,3 +11,14 @@ z = "awesome"
 print(x + y + z)
 
 #Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome"
+
+#For numbers, the + character works as a mathematical operator
+x = 5
+y = 10
+print(x + y)
+
+#In the print() function, when you try to combine a string and a number with the + operator, 
+# Python will give you an error
+x = 5
+y = "John"
+print(x , y)
