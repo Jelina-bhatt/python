a4f7d1ccb607776febc82b9dc509d9552b3de316
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-15 20:38:54 +0545

    output variables

---
+++ variable/output_variables.py
@@ -0,0 +1,11 @@
+#In the print() function, you output multiple variables, separated by a comma
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#You can also use the + operator to output multiple variables
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
