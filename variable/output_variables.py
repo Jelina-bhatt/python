7163262570ce0ea6f7012e0921f80ab751e0806c
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-15 20:40:56 +0545

    output variables modified

--- variable/output_variables.py
+++ variable/output_variables.py
@@ -2,10 +2,12 @@
 x = "Python"
 y = "is"
 z = "awesome"
-print(x, y, z)
+print(x,y,z)
 
 #You can also use the + operator to output multiple variables
 x = "Python "
 y = "is "
 z = "awesome"
 print(x + y + z)
+
+#Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome"
