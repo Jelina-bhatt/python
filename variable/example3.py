3dafa0fff2d0bdbb27dc12071d68da1afbe9b1ac
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-15 20:20:49 +0545

    init commit

---
+++ variable/example3.py
@@ -0,0 +1,8 @@
+#If you want to specify the data type of a variable, this can be done with casting.
+#You can get the data type of a variable with the type() function.
+x=5 #x is of type int
+y="john" #y is now of type str
+print(x) 
+print(y) 
+print(type(x))
+print(type(y))
