6e966eb069c8376646015fdc7b748c344908ca1b
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-16 11:18:39 +0545

    python castint committed

---
+++ python casting/casting.py
@@ -0,0 +1,34 @@
+#Casting in python is therefore done using constructor functions
+
+#Integers:
+
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+print(x)
+print(y)
+print(z)
+
+
+#for floats:
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+#for strings
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+print(type(x))
+
+x=complex(1)
+print(x)
