4b3ad5c415ec60ceef8540e5325b3d2cdd204616
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-16 10:15:03 +0545

    data type in python committed

---
+++ data type/data-type.py
@@ -0,0 +1,12 @@
+#You can get the data type of any object by using the type() function
+x="hello world"
+#print x
+print(x)
+#print data type of x
+print(type(x))
+
+x=3
+#print x
+print(x)
+#print data type of x
+print(type(x))
