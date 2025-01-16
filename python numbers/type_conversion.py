049d6a4271bbd3675c54607c39f439044b502d51
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-16 11:01:26 +0545

    type conversion committed

---
+++ python numbers/type_conversion.py
@@ -0,0 +1,22 @@
+#You can convert from one type to another with the int(), float(), and complex() methods:
+
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
