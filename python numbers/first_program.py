b91a89574169bb1d1865ef0183feeb2709f476bc
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-28 13:14:25 +0545

    init changes

--- python numbers/first_program.py
+++ python numbers/first_program.py
@@ -1,3 +1,8 @@
+'''
+ctrl + / is used to uncomment lines
+is used to comment lines 
+we can write \n to escape lines in print statements
+'''
 print("hello world")
 print(90)
 print("90")
@@ -5,3 +10,4 @@ print("hello world",7)
 print(70*8)
 print(70+90)
 print(110-90)
+
