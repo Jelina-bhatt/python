2a0a3cafa8fc9aca54fdb1319e754c40e62bbf5d
jelina bhatt <jelinabhatt1@gmail.com>
2025-01-28 13:20:53 +0545

    escape, sep and end

---
+++ folder1/escape-N-sep-end.py
@@ -0,0 +1,5 @@
+#in this we will learn about escape, sep, end varirables
+print("jelina", 24, sep="+",end="009")
+print("hello world")
+#sep is used to separate the previous strings,num by special symbol
+#end is used to separate two statement by some digits
