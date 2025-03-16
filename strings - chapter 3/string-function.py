05a5d9975110e2f9ae28fef77875cb8168b6f68f
jelina bhatt <jelinabhatt1@gmail.com>
2025-03-16 23:32:46 +0545

    string function

--- strings - chapter 3/string-function.py
+++ strings - chapter 3/string-function.py
@@ -1,4 +1,22 @@
-name="radhekrishn"
+name="radhe Krishn  "
 
 print(len(name))
 print(name.endswith("shna"))
+print(name.startswith("radhe"))
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(name.find("radhe"))
+print(name.strip())
+print(name.split())
+
+s = "  Python is awesome!  "
+
+print(s.lower())     # '  python is awesome!  '
+print(s.upper())     # '  PYTHON IS AWESOME!  '
+print(s.strip())     # 'Python is awesome!' (removes whitespace)
+print(s.replace("Python", "Java"))  # '  Java is awesome!  '
+print(s.split())     # ['Python', 'is', 'awesome!'] (splits by space)
+print(s.startswith("Py"))  # True
+print(s.endswith("!"))     # True
+print(s.find("is"))        # 9 (index of first occurrence)
