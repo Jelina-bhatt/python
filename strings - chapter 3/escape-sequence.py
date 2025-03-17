40748d661ae7ff7c56ba2963042a338d1e0ead30
jelina bhatt <jelinabhatt1@gmail.com>
2025-03-17 08:30:15 +0545

    escape sequence in strings

---
+++ strings - chapter 3/escape-sequence.py
@@ -0,0 +1,8 @@
+a="iam a greatest devotee of shree krishn \n but iam not being able to perform devotion at this point of time"
+b="\n what should i do now"
+print(a+b)
+# in python \n is used to print statement in next line
+
+c="shreemati radharani has served me alot in this journey \"jai shree radha shyaam\""
+# in python to use quotation mark in any particular word we use above sequence
+print(c)
