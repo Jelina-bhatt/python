fd6c56d6c12a9b08fe313e211d6d348d1d3e8cad
Jelina <jelinabhatt1@gmail.com>
2025-07-05 17:44:04 +0545

    break and continue

---
+++ loops/code/break_continue.py
@@ -0,0 +1,13 @@
+#for break 
+for i in range(0,100):
+       if(i==34):
+               break  #exit the loop right now
+       print(i)
+     
+
+#for continue
+for i in range(0,100):
+       if(i==34):
+            continue  #skip the iteration
+       print(i)
+     
