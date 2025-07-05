6c9eff3ed2255e5392afa0fa659c317893a5cb6c
Jelina <jelinabhatt1@gmail.com>
2025-07-05 16:40:56 +0545

    loops with lists, tuples and strings

---
+++ loops/code/for_loop_iterate.py
@@ -0,0 +1,14 @@
+#for loop in lists
+j=[1,3,4,5]
+for i in j:
+        print(i)
+
+#for loops in tuples
+t=(6,4,24,545)
+for i in t:
+        print(i)
+
+#for loops with strings
+d="jelina"
+for i in d:
+        print(i)
