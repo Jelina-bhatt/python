49b88a09f9cfa8f1c32477bfa6422b574eb6e750
Jelina <jelinabhatt1@gmail.com>
2025-09-18 23:26:11 +0545

    functions

---
+++ functions_and_recursions/code/simple.py
@@ -0,0 +1,19 @@
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+    average = (a + b + c) / 3
+    print("Average:", average)
+
+# These calls should be outside the function
+avg()
+print("Thank you")
+
+avg()
+print("Thank you")
+
+avg()
+print("Thank you")
+
+avg()
+print("Thank you")
---
+++ loops/practice set/good_day.py
@@ -0,0 +1,4 @@
+for i in range(31):
+    print("good morning")
+
+  
