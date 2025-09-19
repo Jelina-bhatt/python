ccb78bbb4d9d21773a9d46ad0f7eaee29c6a9238
Jelina <jelinabhatt1@gmail.com>
2025-09-19 15:08:01 +0545

    conversion of inches to meter

---
+++ functions_and_recursions/practice set/conversion_inch-cms.py
@@ -0,0 +1,8 @@
+# this program converts inches to centimeters
+
+def inch_to_cms(inch):
+    return inch*2.54
+
+
+n= int(input("enter the values in inches:"))
+print(f"the corresponding value in cms is :{inch_to_cms(n)}")
