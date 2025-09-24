576b779143b3ded95ef12062fb21cff78031e8a0
Jelina <jelinabhatt1@gmail.com>
2025-09-24 21:57:10 +0545

    files

--- filepy/code/file.py
+++ filepy/code/file.py
@@ -1,1 +1,5 @@
- 
+# To print what is written in txt file
+f=open("myfile.txt","r")
+data =f.read()
+print(data)
+f.close()
---
+++ filepy/code/problem_02.py
@@ -0,0 +1,4 @@
+st="hey jellu i missed you"
+f=open("myfile.txt","w")
+f.write(st)
+f.close()
